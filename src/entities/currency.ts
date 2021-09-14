5099976f5ab9497cb908c402a550bbd1d7e1a39e
0xdon <0xdon@protonmail.com>
2021-09-14 13:00:28 +0100

    replaced avax with matic as default CAVAX

--- src/entities/currency.ts
+++ src/entities/currency.ts
@@ -16,7 +16,7 @@ export class Currency {
   /**
    * The only instance of the base class `Currency`.
    */
-  public static readonly CAVAX: Currency = new Currency(18, 'AVAX', 'Avalanche')
+  public static readonly CAVAX: Currency = new Currency(18, 'MATIC', 'Matic')
 
   /**
    * Constructs an instance of the base class `Currency`. The only instance of the base class `Currency` is `Currency.ETHER`.
