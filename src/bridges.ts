a068efe3afa2a31383f9c3ec922759fe84af639e
Bilal Corbacioglu <bilalcorbacioglu@gmail.com>
2022-11-14 10:30:13 +0000

    fix: code refactoring

--- src/bridges.ts
+++ src/bridges.ts
@@ -3,7 +3,6 @@ export interface Bridge {
   name: string
   logo: string
   aggregator_address: string
-  lim: string
   affiliate: string
   fee: number
 }
@@ -13,31 +12,28 @@ export const THORSWAP: Bridge = {
   name: 'ThorSwap',
   logo: 'https://raw.githubusercontent.com/pangolindex/sdk/master/src/images/bridges/thorchain.png',
   aggregator_address: '0x7a68c37D8AFA3078f3Ad51D98eA23Fe57a8Ae21a',
-  lim: '100001618',
   affiliate: 'thor1h2hf0txuzdl2hedx9epcqc6zf65aldk4ckpwcw',
   fee: 10
 }
 
-//TODO: edit this
+//TODO:
 export const LIFI: Bridge = {
   id: 'lifi',
   name: 'LI.FI',
   logo: 'https://raw.githubusercontent.com/pangolindex/sdk/master/src/images/bridges/lifi.png',
-  aggregator_address: '0x7a68c37D8AFA3078f3Ad51D98eA23Fe57a8Ae21a',
-  lim: '100001618',
-  affiliate: 'thor1h2hf0txuzdl2hedx9epcqc6zf65aldk4ckpwcw',
-  fee: 10
+  aggregator_address: '',
+  affiliate: '',
+  fee: 0
 }
 
-//TODO: edit this
+//TODO:
 export const SQUID: Bridge = {
   id: 'squid',
   name: 'Squid',
-  logo: 'https://raw.githubusercontent.com/pangolindex/sdk/master/src/images/bridges/lifi.png',
-  aggregator_address: '0x7a68c37D8AFA3078f3Ad51D98eA23Fe57a8Ae21a',
-  lim: '100001618',
-  affiliate: 'thor1h2hf0txuzdl2hedx9epcqc6zf65aldk4ckpwcw',
-  fee: 10
+  logo: 'https://raw.githubusercontent.com/pangolindex/sdk/master/src/images/bridges/squid.png',
+  aggregator_address: '',
+  affiliate: '',
+  fee: 0
 }
 
 export const BRIDGES: Bridge[] = [LIFI, THORSWAP, SQUID]
