be4a88f88e14d463be5d1025a77822ead2c7a706
Bilal Corbacioglu <bilalcorbacioglu@gmail.com>
2023-06-05 16:50:14 +0100

    fix: changed old pool init code hash

--- src/elixir/constants.ts
+++ src/elixir/constants.ts
@@ -20,7 +20,7 @@ export const TICK_SPACINGS: { [amount in FeeAmount]: number } = {
   [FeeAmount.HIGH]: 200
 }
 
-export const POOL_INIT_CODE_HASH = '0x3fcff66860e53a77b6570a354f8e175e3e01fb76620272df5c59e6cf9bffc577'
+export const POOL_INIT_CODE_HASH = '0x41a723f9e6457830b1b7a44df4435fab88581d073226894b33131815dd674c22'
 
 export const POOL_INIT_CODE_HASH_MAPPING: { [chainId in ChainId]: string } = {
   [ChainId.FUJI]: POOL_INIT_CODE_HASH,
@@ -49,8 +49,8 @@ export const POOL_INIT_CODE_HASH_MAPPING: { [chainId in ChainId]: string } = {
   [ChainId.MOONBEAM]: '',
   [ChainId.OP]: '',
   [ChainId.COSTON2]: POOL_INIT_CODE_HASH,
-  [ChainId.EVMOS_TESTNET]: '0x41a723f9e6457830b1b7a44df4435fab88581d073226894b33131815dd674c22',
-  [ChainId.EVMOS_MAINNET]: '0x41a723f9e6457830b1b7a44df4435fab88581d073226894b33131815dd674c22'
+  [ChainId.EVMOS_TESTNET]: POOL_INIT_CODE_HASH,
+  [ChainId.EVMOS_MAINNET]: POOL_INIT_CODE_HASH
 }
 
 export enum NumberType {
