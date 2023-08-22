5dbed5cf40edfad81902f6806ecd657210d75608
Bilal Corbacioglu <bilalcorbacioglu@gmail.com>
2023-08-22 12:27:52 +0300

    feat: added elixirVault providers

---
+++ src/elixirVaults.ts
@@ -0,0 +1,17 @@
+export interface ElixirVaultProvider {
+  id: string
+  name: string
+  logo: string
+}
+
+export const DEFIEDGE: ElixirVaultProvider = {
+  id: 'defiedge',
+  name: 'DefiEdge',
+  logo: 'https://raw.githubusercontent.com/pangolindex/sdk/master/src/images/elixirVault/DefiEdge.svg'
+}
+
+export const ICHI: ElixirVaultProvider = {
+  id: 'ichi',
+  name: 'Ichi',
+  logo: 'https://raw.githubusercontent.com/pangolindex/sdk/master/src/images/elixirVault/Ichi.svg'
+}
