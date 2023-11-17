944a2844a24115f696854c30a7a8bde5896993e1
Pedro Antonio <37405304+prodesert22@users.noreply.github.com>
2023-11-16 23:03:10 -0400

    fix: flare to compound to new pfl/flr farm

--- src/chains.ts
+++ src/chains.ts
@@ -1712,7 +1712,8 @@ export const FLARE_MAINNET: Chain = {
     mini_chef: {
       address: '0xe19a6a2253B40F884FcaE7945C3edbB5AAf8cb60',
       active: true,
-      type: ChefType.PANGO_CHEF
+      type: ChefType.PANGO_CHE,
+      compoundPoolIdForNonPngFarm: 4
     },
     airdrop: {
       address: '0x290593624b60D5c8EBf329344343d2cD21c46Cf3',
